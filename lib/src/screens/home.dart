a146d02da227d2cd8a32ec86bbb07b581734b0dc
Jonas <jwallat@hotmail.de>
2018-12-09 21:21:30 +0100

    added cat and box

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -16,18 +16,55 @@ class HomeState extends State<Home> with TickerProviderStateMixin {
       duration: Duration(seconds: 2),
       vsync: this,
     );
+    catAnimation = Tween(begin: 0.0, end: 100.0).animate(
+      CurvedAnimation(
+        parent: catController,
+        curve: Curves.easeIn,
+      ),
+    );
+  }
+
+  void onTap() {
+    if (catController.status == AnimationStatus.completed) {
+      catController.reverse();
+    } else if (catController.status == AnimationStatus.dismissed) {
+      catController.forward();
+    }
   }
 
   Widget build(context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Animation!"),
-      ),
-      body: buildAnimation(),
+        appBar: AppBar(
+          title: Text("Animation!"),
+        ),
+        body: GestureDetector(
+          child: Center(
+            child: Stack(
+              children: [buildCatAnimation(), buildBox()],
+            ),
+          ),
+          onTap: onTap,
+        ));
+  }
+
+  Widget buildCatAnimation() {
+    return AnimatedBuilder(
+      animation: catAnimation,
+      builder: (context, child) {
+        return Container(
+          child: child,
+          margin: EdgeInsets.only(top: catAnimation.value),
+        );
+      },
+      child: Cat(),
     );
   }
 
-  Widget buildAnimation() {
-    return Cat();
+  Widget buildBox() {
+    return Container(
+      height: 200.0,
+      width: 200.0,
+      color: Colors.brown,
+    );
   }
 }
